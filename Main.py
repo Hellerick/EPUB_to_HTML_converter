8ae7aae7a8d28671018eddb2cbe14e297ae1d14b
Hellerick <hellerickferlibay@gmail.com>
2017-02-10 13:45:38 +0700

    From Severboat

---
+++ Main.py
@@ -0,0 +1,8 @@
+epub_path = r"C:\Users\User\PycharmProjects\EPUB_to_HTML_converter\Examples\Ginei_01_EN.epub"
+
+def convert_epub_to_html(epub_path):
+    print(epub_path)
+    pass
+
+if __name__ == '__main__':
+    convert_epub_to_html(epub_path)
