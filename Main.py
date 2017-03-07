04aea01506ed666d28ea9e18cf7c72f545ea635d
Hellerick <hellerickferlibay@gmail.com>
2017-03-07 16:07:16 +0700

    From Severboat

--- Main.py
+++ Main.py
@@ -40,12 +40,45 @@ def convert_epub_to_html(epub_path):
     print('Contents', contents_tree)
     spine = contents_tree.find('{http://www.idpf.org/2007/opf}spine')
     # for c in contents_tree.iter(): print(c)
-    print(spine)
+    parts = []
     for item in spine.iter():
-        print
+        # print(item.attrib)
+        if 'idref' in item.attrib:
+            parts += [item.attrib['idref']]
+    print(parts)
+    compiled_text = ''
+    for p in parts:
+        item_subpath = contents_tree.find('.//{http://www.idpf.org/2007/opf}item[@id="'+p+'"]').attrib['href']
+        with open(os.path.join(contents_folder, item_subpath), mode='rt', encoding='utf8') as f:
+            print(os.path.join(contents_folder, item_subpath))
+            compiled_text += f.read()
+    compiled_text = re.sub(r'</body>(.|\n)*?<body[^>]*>', '', compiled_text)
+    css_file_path = contents_tree.find('.//{http://www.idpf.org/2007/opf}item[@id="css"]').attrib['href']
+    compiled_text = re.sub(r'<style[^>]*>.*</style>', '<link href="'+css_file_path+'" rel="stylesheet" type="text/css"/>', compiled_text, re.DOTALL)
+    # <item href="stylesheet.css" id="css" media-type="text/css"/>
+    result_file_path = re.sub(r'(\.epub)?\Z', '.html', epub_path)
+    with open(result_file_path, mode='wt', encoding='utf8') as f:
+        f.write(compiled_text)
+        print(len(compiled_text))
 
+# <link href="page_styles.css" rel="stylesheet" type="text/css"/>
 
-    # content.opf # spine -- default reading order
+        # content.opf # spine -- default reading order
 
 if __name__ == '__main__':
     convert_epub_to_html(epub_path)
+
+'''
+</body>
+</html>
+<?xml version='1.0' encoding='utf-8'?>
+<html xmlns="http://www.w3.org/1999/xhtml">
+  <head>
+    <title>Preview Of Artemis Fowl: The Arctic Incident</title>
+    <meta name="Inept.resource" content="urn:uuid:00000000-0000-0000-0000-000000000000"/>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <link href="../stylesheet.css" rel="stylesheet" type="text/css"/>
+<link href="../page_styles.css" rel="stylesheet" type="text/css"/>
+</head>
+  <body class="calibre">
+'''
